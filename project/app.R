fe7f603c7bb7c2226a86f55e1bbf1000851097c4
Raquel <100399718@alumnos.uc3m.es>
2019-03-13 10:19:01 +0100

    change in points

--- project/app.R
+++ project/app.R
@@ -137,7 +137,7 @@ server <- function(input, output) {
     par(mar = c(5.1, 4.1, 0, 1))
     plot(selectedData(),
          col = clusters()$cluster,
-         pch = 20, cex = 3)
+         pch = 20, cex = 0.5)
     points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
   })
   
