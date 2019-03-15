7cb41fb3d25ad1bf258c1bb7a4f8b17e8deddd9f
Raquel <100399718@alumnos.uc3m.es>
2019-03-15 18:22:53 +0100

    before merge

--- project/app.R
+++ project/app.R
@@ -13,11 +13,13 @@
 # install_github('rCharts', 'ramnathv')
 # install.packages("shinyjs")
 
-
+library(tidyverse)
 library(shiny)
 library(DT)
 library(mice)
 library(BH)
+library(ggplot2)
+library(scales)
 library(devtools)
 library(Rcpp)
 library(rCharts)
@@ -29,9 +31,14 @@ library(dplyr)
 url="http://halweb.uc3m.es/esp/Personal/personas/imolina/esp/Archivos/VinhoVerdeQuality_Data.csv"
 
 vino=read.csv(url,header=TRUE,sep=";")
-vino=vino[,4:dim(vino)[2]]
-vino=na.omit(vino)
-scale_vino=vino
+vino <- as_tibble(vino)
+vino=vino %>%select(-starts_with("X"))
+# vino=vino %>% select('fixed.acidity','volatile.acidity',"citric.acid","residual.sugar","chlorides",
+#                      "free.sulfur.dioxide","total.sulfur.dioxide","density","pH",
+#                      "sulphates","alcohol", "quality", "Variant", "Taste")
+vino=vino %>%  na.omit()
+
+#PUT THE SCALE IN THE CORRECT WAY
 for(i in 1:dim(vino)[2]){
   if(is.numeric(vino[,i])==TRUE) scale_vino[,i]=scale(vino[,i])
 }
@@ -39,25 +46,33 @@ for(i in 1:dim(vino)[2]){
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("Vinho Verde Wine EXPLORER",
-                 # # Application title
-                 # tabPanel("Old Faithful Geyser Data",
-                 #          
-                 #          # Sidebar with a slider input for number of bins 
-                 #          sidebarLayout(position="right",
-                 #                        sidebarPanel(
-                 #                          #actionButton("download", "Download the data set"),
-                 #                          sliderInput("bins",
-                 #                                      "Number of bins:",
-                 #                                      min = 1,
-                 #                                      max = 50,
-                 #                                      value = 30)),
-                 #                        # Show a plot of the generated distribution
-                 #                        mainPanel(
-                 #                          plotOutput("distPlot")
-                 #                        )
-                 # 
-                 #                                              )
-                 # ),
+                 
+                 tabPanel("Correlation between two variables",
+                          
+                          selectInput('xcol1', label = 'X Variable', choices = names(vino)),
+                          selectInput('ycol1', label = 'Y Variable', choices = names(vino),selected = "volatile.acidity"),
+                          
+                          mainPanel(
+                            plotOutput('plot2')
+                          )
+                 ),
+                 
+                 tabPanel("Pie Chart-Variant",
+                          
+                          selectInput('variant.pie', label = 'Variant', choices = unique(as.character(vino$Variant))),
+                          mainPanel(
+                            plotOutput('plot3')
+                          )
+                 ),
+                 
+                 tabPanel("Pie Chart-Quality",
+                          
+                          selectInput('quality.pie', label = 'Quality', choices = unique(as.character(vino$Variant))),
+                          
+                          mainPanel(
+                            plotOutput('plot4')
+                          )
+                 ),
                  
                  tabPanel('Vinho Verde k-means clustering',
                      selectInput('xcol', 'X Variable', names(vino)),
@@ -78,24 +93,17 @@ ui <- navbarPage("Vinho Verde Wine EXPLORER",
                                       max = max(vino$alcohol), step = 0.1, value = c(10,12),
                                       animate = T, dragRange = T),
                                 sliderInput("quality", label = ("Quality:"), min = min(vino$quality),
-                                      max = max(vino$quality), step = 0.1, value = c(5,8),
+                                      max = max(vino$quality), step = 1, value = c(5,8),
                                       animate = T, dragRange = T),
 
                                 checkboxGroupInput("checkGroup2", label = ("Taste:"),
                                       choices = c(unique(as.character(vino$Taste))),
                                       selected = c(unique(as.character(vino$Taste)))),
+                                
+                                checkboxInput("all","Select All/None", value=TRUE),
                             # Download Button
                             downloadButton("downloadData", "Download Selection"),
-                            downloadButton("downloadData2", "Download Dataset"),
-                            
-                            
-                            actionButton(inputId = "clearAllBottom",
-                                         label = "Clear selection",
-                                         icon = icon("square-o")),
-                            actionButton(inputId = "selectAllBottom",
-                                         label = "Select all",
-                                         icon = icon("check-square-o")),
-                            uiOutput("tasteControl")
+                            downloadButton("downloadData2", "Download Dataset")
                           ),
                       
                       mainPanel(
@@ -125,6 +133,14 @@ ui <- navbarPage("Vinho Verde Wine EXPLORER",
 # Define server logic required to draw a histogram
 server <- function(input, output,session) {
   
+  # select/deselect all using action button
+  observe({
+    updateCheckboxGroupInput(session=session, 
+                             inputId="checkGroup2",
+                             choices = unique(as.character(data$Taste)),
+                             selected = if(input$all) unique(as.character(data$Taste)))
+  })
+  
   # Reactive value for selected dataset ----
   datasetInput <- reactive({
     data <- vino
@@ -150,96 +166,152 @@ server <- function(input, output,session) {
   output$table <- DT::renderDataTable(DT::datatable({
     data <- vino
     if (input$variant != "All") {
-      data <- data[data$Variant == input$variant,]
-    }
+      data <- data[data$Variant == input$variant,]}
     
     #Choose the correct alcohol interval
-    data=data[which(data$alcohol>min(input$alcohol) & data$alcohol<max(input$alcohol)),]
-    
+    data=data %>% filter(data$alcohol>min(input$alcohol)& data$alcohol<max(input$alcohol))    
     #Choose the correct quality interval
-    data=data[which(data$quality>min(input$quality) & data$quality<max(input$quality)),]
+    data=data %>% filter(data$quality>min(input$quality) & data$quality<max(input$quality))    
     
-    # if(input$checkgroup != "All"){
-    #   data=data[which(data$Taste == input$checkgroup),]
-    # }
-
+    data=data[data$Taste == input$checkGroup2,]
+    
     data
-    
   }))
   
-  # # Initialize reactive values
-  # taste <- sort(unique(data$Taste))
-  # values <- reactiveValues()
-  # values$taste <- taste
-  # 
-  # 
-  # observe({
-  #   if(input$selectAllTop > 0) {
-  #     updateCheckboxGroupInput(session=session, inputId="taste",
-  #                              choices=taste, selected=taste)
-  #     values$taste <- taste
-  #   }
-  # })
-  # observe({
-  #   if(input$clearAllBottom > 0) {
-  #     updateCheckboxGroupInput(session=session, inputId="taste",
-  #                              choices=taste, selected=taste)
-  #     values$taste <- taste
-  #   }
-  # })
-  # 
-  # observe({
-  #   if(input$clearAllTop > 0) {
-  #     updateCheckboxGroupInput(session=session, inputId="taste",
-  #                              choices=taste, selected=NULL)
-  #     values$taste <- c()
-  #   }
-  # })
-  # observe({
-  #   if(input$clearAllBottom > 0) {
-  #     updateCheckboxGroupInput(session=session, inputId="taste",
-  #                              choices=taste, selected=NULL)
-  #     values$taste <- c()
-  #   }
-  # })
+  #Correlation plot
+  output$plot2 <- renderPlot({
+    
+    ggplot(vino, aes_string(x=input$xcol1, y=input$ycol1, color=vino$Taste)) +
+      geom_point(size=2, shape=23)+
+      geom_smooth(method="lm", se=TRUE, fullrange=TRUE)
+  })
+  
+  
+  #Piechart variant
+  output$plot3 <- renderPlot({
+  
+  if (input$variant.pie=="red"){
+    x1=nrow(vino %>% filter(Taste== 'Balanced', Variant=="red")) 
+    x2=nrow(vino %>% filter(Taste== 'Light-Bodied', Variant=="red")) 
+    x3=nrow(vino %>% filter(Taste== 'Low acid', Variant=="red"))
+    x4=nrow(vino %>% filter(Taste== 'Sweet', Variant=="red"))
+    x5=nrow(vino %>% filter(Taste== 'Very low acid', Variant=="red"))
+    #total.red=sum((vino$Variant)=="red")
+    total.red=nrow(vino %>% filter(Variant=="red"))
+    value = c(x1,x2,x3,x4,x5)/total.red
+    
+    df <- data.frame(Taste = c("Balanced", "Light-Bodied", "Low acid", "Sweet", "Very low acid"),
+                     value = c(x1,x2,x3,x4,x5)/total.red)
+    ggplot(df, aes(x="", y=value, fill=Taste))+
+      geom_bar(width = 1, stat = "identity")+
+      coord_polar("y", start=0)+
+      geom_text(aes(y = value), 
+                label = percent(value), size=3) 
+    
+  }else if (input$variant.pie=="white"){
+    x1=nrow(vino %>% filter(Taste== 'Balanced', Variant=="white")) 
+    x2=nrow(vino %>% filter(Taste== 'Light-Bodied', Variant=="white")) 
+    x3=nrow(vino %>% filter(Taste== 'Low acid', Variant=="white"))
+    x4=nrow(vino %>% filter(Taste== 'Sweet', Variant=="white"))
+    x5=nrow(vino %>% filter(Taste== 'Very low acid', Variant=="white"))
+    total.white=nrow(vino %>% filter(Variant=="white"))
+    value = c(x1,x2,x3,x4,x5)/total.white
+    
+    df <- data.frame(Taste = c("Balanced", "Light-Bodied", "Low acid", "Sweet", "Very low acid"),
+                     value = c(x1,x2,x3,x4,x5)/total.white)
+    ggplot(df, aes(x="", y=value, fill=Taste))+
+      geom_bar(width = 1, stat = "identity")+
+      coord_polar("y", start=0)+
+      geom_text(aes(y = value), 
+                label = percent(value), size=3) 
+  }
+})
 
+#Piechart quality
+output$plot4 <- renderPlot({
   
-  # Event type checkbox
-  # output$tasteControl <- renderUI({
-  #   checkboxGroupInput('taste', 'Taste:',
-  #                      taste, selected = values$taste)
-  # })
-  
-  observe({
-    x <- input$selectAllButton
+  if (input$quality.pie=="red"){
     
-    # Can use character(0) to remove all choices
-    if (is.null(x))
-      x <- character(0)
     
-    # Can also set the label and select items
-    updateCheckboxGroupInput(session, "CheckboxGroup2",
-                             # label = paste("Checkboxgroup label", length(x)),
-                             choices = c(unique(as.character(vino$Taste))),
-                             selected = c(unique(as.character(vino$Taste)))
-    )
-  })
-  
-  observe({
-    x <- input$clearAllButton
+    # x3=sum((vino$quality)==3 & (vino$Variant)=="red")
+    # x4=sum((vino$quality)==4 & (vino$Variant)=="red")
+    # x5=sum((vino$quality)==5 & (vino$Variant)=="red")
+    # x6=sum((vino$quality)==6 & (vino$Variant)=="red")
+    # x7=sum((vino$quality)==7 & (vino$Variant)=="red")
+    # x8=sum((vino$quality)==8 & (vino$Variant)=="red")
+    # x9=sum((vino$quality)==9 & (vino$Variant)=="red")
+    # total.red=sum((vino$Variant)=="red")
     
-    # Can use character(0) to remove all choices
-    if (is.null(x)==F)
-      updateCheckboxGroupInput(session, "CheckboxGroup2",
-                             label = paste("Checkboxgroup label", length(x)),
-                             choices = c(unique(as.character(vino$Taste))),
-                             selected = NULL
-    )
-  })
+    x3=nrow(vino %>% filter(quality== 3, Variant=="red"))
+    x4=nrow(vino %>% filter(quality== 4, Variant=="red"))
+    x5=nrow(vino %>% filter(quality== 5, Variant=="red"))
+    x6=nrow(vino %>% filter(quality== 6, Variant=="red"))
+    x7=nrow(vino %>% filter(quality== 7, Variant=="red"))
+    x8=nrow(vino %>% filter(quality== 8, Variant=="red"))
+    x9=nrow(vino %>% filter(quality== 9, Variant=="red"))
+    total.red=nrow(vino %>% filter(Variant=="red"))
+    value = c(x3,x4,x5,x6,x7,x8,x9)/total.red
+    
+    df <- data.frame(Quality = c("Quality=3", "Quality=4", "Quality=5", "Quality=6", 
+                                 "Quality=7","Quality=8","Quality=9"),
+                     value = c(x3,x4,x5,x6,x7,x8,x9)/total.red)
+    ggplot(df, aes(x="", y=value, fill=Quality))+
+      geom_bar(width = 1, stat = "identity")+
+      coord_polar("y", start=0)+
+      geom_text(aes(y = value), 
+                label = percent(value), size=3) 
+    
+  }else if (input$quality.pie=="white"){
+    
+    
+    # x3=sum((vino$quality)==3 & (vino$Variant)=="white")
+    # x4=sum((vino$quality)==4 & (vino$Variant)=="white")
+    # x5=sum((vino$quality)==5 & (vino$Variant)=="white")
+    # x6=sum((vino$quality)==6 & (vino$Variant)=="white")
+    # x7=sum((vino$quality)==7 & (vino$Variant)=="white")
+    # x8=sum((vino$quality)==8 & (vino$Variant)=="white")
+    # x9=sum((vino$quality)==9 & (vino$Variant)=="white")
+    # total.white=sum((vino$Variant)=="white")
+    
+    x3=nrow(vino %>% filter(quality== 3, Variant=="white"))
+    x4=nrow(vino %>% filter(quality== 4, Variant=="white"))
+    x5=nrow(vino %>% filter(quality== 5, Variant=="white"))
+    x6=nrow(vino %>% filter(quality== 6, Variant=="white"))
+    x7=nrow(vino %>% filter(quality== 7, Variant=="white"))
+    x8=nrow(vino %>% filter(quality== 8, Variant=="white"))
+    x9=nrow(vino %>% filter(quality== 9, Variant=="white"))
+    total.white=nrow(vino %>% filter(Variant=="white"))
+    value = c(x3,x4,x5,x6,x7,x8,x9)/total.white
+    
+    df <- data.frame(Quality = c("Quality=3", "Quality=4", "Quality=5", "Quality=6", 
+                                 "Quality=7","Quality=8","Quality=9"),
+                     value = c(x3,x4,x5,x6,x7,x8,x9)/total.white)
+    ggplot(df, aes(x="", y=value, fill=Quality))+
+      geom_bar(width = 1, stat = "identity")+
+      coord_polar("y", start=0)+
+      geom_text(aes(y = value), 
+                label = percent(value), size=3) 
+    
+  }
   
-    output$property <- renderPrint({input$checkGroup})
+})
+
+
+output$distPlot <- renderPlot({
+  # generate bins based on input$bins from ui.Rt
+  x    <- faithful[, 2] 
+  bins <- seq(min(x), max(x), length.out = input$bins + 1)
   
-  output$taste <- renderPrint({input$checkGroup2})
+  # draw the histogram with the specified number of bins
+  hist(x, breaks = bins, col = 'darkgray', border = 'white')
+})
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+  
+  
+  output$property <- renderPrint({input$checkGroup})
+  
   
   # output$property <- renderPlot({
   #  data <- vino
@@ -276,40 +348,17 @@ server <- function(input, output,session) {
          pch = 20, cex = 0.5)
     points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
   })
-  
-  
-  
-  # output$distPlot <- renderPlot({
-  #   # generate bins based on input$bins from ui.Rt
-  #   x    <- faithful[, 2] 
-  #   bins <- seq(min(x), max(x), length.out = input$bins + 1)
-  #   
-  #   # draw the histogram with the specified number of bins
-  #   hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  # })
-  
-  # Downloadable csv of selected dataset ----
-  
+
   #Selected data download
   output$downloadData <- downloadHandler(
     filename = function() {
-      # data <- vino
-      # if (input$variant != "All") {
-      #   data <- data[data$Variant == input$variant,]
-      # }
-      # 
-      # #Choose the correct alcohol interval
-      # data=data[which(data$alcohol>min(input$alcohol) & data$alcohol<max(input$alcohol)),]
-      # 
-      # #Choose the correct quality interval
-      # data=data[which(data$quality>min(input$quality) & data$quality<max(input$quality)),]
-      # 
       paste(input$data, ".csv", sep = "")
     },
     content = function(file) {
       write.csv(datasetInput(), file, row.names = TRUE)
     }
   )
+  
   #Full data download
   output$downloadData2 <- downloadHandler(
     filename = function() {
@@ -320,6 +369,17 @@ server <- function(input, output,session) {
     }
   )
 }
+
+# output$distPlot <- renderPlot({
+#   # generate bins based on input$bins from ui.Rt
+#   x    <- faithful[, 2] 
+#   bins <- seq(min(x), max(x), length.out = input$bins + 1)
+#   
+#   # draw the histogram with the specified number of bins
+#   hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# })
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
